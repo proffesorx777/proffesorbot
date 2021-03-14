4e01f60e97704336e02c41fd0221885b22d1921a
HITMAN | KARTIK <74542005+kartikrajofficial0773h@users.noreply.github.com>
2021-03-14 15:04:31 +0530

    Update startup.sh

--- resources/startup.sh
+++ resources/startup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#    TeleBot - UserBot
-#    Copyright (C) 2021 TeleBot
+#    KINGBot - UserBot
+#    Copyright (C) 2021 KINGBOT
 
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as published by
